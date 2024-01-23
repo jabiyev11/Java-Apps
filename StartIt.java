f5c3399911579fd4ac1011803839a30b46cf33f9
jabiyev11 <cavidjabiyev@gmail.com>
2024-01-23 12:09:15 +0400

    first commit

---
+++ StartIt.java
@@ -0,0 +1,9 @@
+class anotherApp {
+
+  public static void main(String[] args) {
+    
+    System.out.println("The first parametr is: " + args[0]);
+    System.out.println("The second parametr is: " + args[1]);
+       
+  }
+}
