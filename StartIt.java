0f580935c0d04edd3c78ecd039511fb92abed8e5
jabiyev11 <jjabiyev19615@ada.edu.az>
2024-01-23 12:39:23 +0400

    Test commit

--- StartIt.java
+++ StartIt.java
@@ -3,7 +3,7 @@ class anotherApp {
   public static void main(String[] args) {
     
     System.out.println("The first parametr is: " + args[0]);
-    System.out.println("The second parametr is: " + args[1]);
+    System.out.println("The second parametrrrrrrrrrrrrrrrrrrrrrrrrr is: " + args[1]);
        
   }
 }
